672ddf77c9999b062c6864ba4778ee5d5bbf637f
stm-dev-bit <seanstmarshall@gmail.com>
2024-08-08 21:02:37 +0330

    :sparkles: react-query and mui initialized

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import ReactQueryProvider from "./providers/ReactQueryProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ReactQueryProvider>
+          <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+        </ReactQueryProvider>
+      </body>
     </html>
   );
 }
---
+++ src/app/providers/ReactQueryProvider.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
+
+function ReactQueryProvider({ children }: React.PropsWithChildren) {
+  const [client] = useState(
+    () =>
+      new QueryClient({ defaultOptions: { queries: { staleTime: 60 * 1000 } } })
+  );
+
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default ReactQueryProvider;
