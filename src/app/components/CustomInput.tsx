4969cc40a2113d2cbfa1a27e3b0d09c539424660
stm-dev-bit <seanstmarshall@gmail.com>
2024-08-09 13:23:27 +0330

    :bug: fix warning

--- src/app/components/CustomInput.tsx
+++ src/app/components/CustomInput.tsx
@@ -1,17 +1,26 @@
+import React from "react";
 import { TextField, TextFieldProps } from "@mui/material";
 
 type CustomInputProps = TextFieldProps & {
   isfullwidth?: boolean;
 };
 
-const CustomInput: React.FC<CustomInputProps> = (props) => {
-  return (
-    <TextField
-      InputLabelProps={{ shrink: true }}
-      fullWidth={props.isfullwidth ?? true}
-      {...props}
-    />
-  );
-};
+/**
+ * CustomInput component for form fields, supports refs with React.forwardRef.
+ */
+const CustomInput = React.forwardRef<HTMLInputElement, CustomInputProps>(
+  (props, ref) => {
+    return (
+      <TextField
+        InputLabelProps={{ shrink: true }}
+        fullWidth={props.isfullwidth ?? true}
+        inputRef={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+CustomInput.displayName = "CustomInput";
 
 export default CustomInput;
